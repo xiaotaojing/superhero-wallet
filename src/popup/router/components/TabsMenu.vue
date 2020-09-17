f6573db6e2d13ddb33e110b959cbf37fae393156
Stanislav Slavov <stanislav@hack.bg>
2020-09-17 14:29:07 +0300

    Created tabs component

---
+++ src/popup/router/components/TabsMenu.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="tabs-menu">
+    <a
+      class="tabs-menu-item"
+      :class="{ active: selected === value }"
+      v-for="{ text, value } in tabOptions"
+      :key="value"
+      :title="text"
+      @click="(selected = value), method($event, value)"
+    >
+      {{ text }}
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    tabOptions: { type: Array, default: null },
+    activeTab: { type: [String, Number], default: '' },
+    method: { type: Function, required: true },
+  },
+  data() {
+    return {
+      selected: this.activeTab,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import '../../../common/variables';
+
+.tabs-menu {
+  padding: 0 15px;
+  background-color: $black-3;
+}
+
+.tabs-menu-item {
+  font-size: 16px;
+  padding: 5.5px 0;
+  margin-right: 17px;
+  text-decoration: none;
+  color: $gray-2;
+  display: inline-block;
+
+  &:hover {
+    color: $gray-1;
+    cursor: pointer;
+  }
+
+  &.active {
+    color: $accent-color;
+    border-bottom: 1px solid $accent-color;
+  }
+
+  &:last-child {
+    margin-right: 0;
+  }
+}
+</style>
